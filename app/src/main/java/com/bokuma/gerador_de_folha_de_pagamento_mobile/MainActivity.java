7d3974e780b692910cfcf6f468da11d0c7d47b6f
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-08-20 20:16:31 -0300

    Início do projeto

---
+++ app/src/main/java/com/bokuma/gerador_de_folha_de_pagamento_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bokuma.gerador_de_folha_de_pagamento_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
