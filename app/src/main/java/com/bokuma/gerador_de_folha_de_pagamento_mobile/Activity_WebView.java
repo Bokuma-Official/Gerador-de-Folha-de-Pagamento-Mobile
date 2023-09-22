eeeab203e239e21a87ce04d3d7ae7f896a6449f6
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-09-21 23:24:34 -0300

    Teste de sites abrindo no modo WebView!

---
+++ app/src/main/java/com/bokuma/gerador_de_folha_de_pagamento_mobile/Activity_WebView.java
@@ -0,0 +1,41 @@
+package com.bokuma.gerador_de_folha_de_pagamento_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Activity_WebView extends AppCompatActivity {
+    WebView wbv_Site;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        wbv_Site=findViewById(R.id.wbv_Site);
+        WebSettings webSettings=wbv_Site.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        wbv_Site.setWebViewClient(new Voltar());
+
+        wbv_Site.loadUrl("https://unip.br");
+    }
+
+    private class Voltar extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(wbv_Site.canGoBack()){
+            wbv_Site.goBack();
+        }
+        else
+            super.onBackPressed();
+    }
+}
